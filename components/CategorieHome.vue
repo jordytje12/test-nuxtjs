e64237b0b19f340a21964bb396749030b0796426
jordytje12 <jordy.vzanten2000@gmail.com>
2024-11-28 21:11:24 +0100

    uploading project

--- components/CategorieHome.vue
+++ components/CategorieHome.vue
@@ -9,10 +9,10 @@ const { data: collections, error } = await useFetch<ShopifyCollectionsResponse>(
 </script>
 
 <template>
-  <h1 class="text-2xl font-bold underline">Collections</h1>
+  <h1 class="text-2xl font-bold underline max-w-7xl mx-auto">Collections</h1>
 
   <template v-if="collections">
-    <ul class="grid grid-cols-3 gap-5">
+    <ul class="grid grid-cols-3 gap-5 max-w-7xl mx-auto">
       <li v-for="collection in collections.data.collections.edges" :key="collection.node.id" class="">
         <img :src="collection.node.image?.src" :alt="collection.node.image?.altText || ''" class="h-64 object-cover min-w-full"/>
         <h2 class="font-semibold">{{ collection.node.title }}</h2>
